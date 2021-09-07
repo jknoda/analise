39c0babf9d6d17e2aa35f6a4763fc023a37415fd
Jorge Noda <jknoda@hotmail.com>
2021-09-07 10:33:04 -0300

    ip minutes

--- src/controllers/General/MeuipController.js
+++ src/controllers/General/MeuipController.js
@@ -19,12 +19,14 @@ module.exports = {
             var mes1 = retorno.data.getMonth();
             var ano1 = retorno.data.getFullYear();
             var hora1 = retorno.data.getHours();
+            var min1 = retorno.data.getMinutes();
             var dia2 = data.getDay();
             var mes2 = data.getMonth();
             var ano2 = data.getFullYear();
             var hora2 = data.getHours();
+            var min2 = data.getMinutes();
 
-            if (dia1 == dia2 && mes1 == mes2 && ano1 == ano2 && hora1 == hora2)
+            if (dia1 == dia2 && mes1 == mes2 && ano1 == ano2 && hora1 == hora2 && min1 == min2)
             {
                 idf = idf - 1;
                 return res.send(idf.toString());
